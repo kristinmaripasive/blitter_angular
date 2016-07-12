9e081a52ff5ff72d286843c08aba3a13593b712d
Kristin Pasive <kristin.pasive@gmail.com>
2016-07-12 16:39:58 -0400

    added state function

--- blitter_angular/js/bleets/new.controller.js
+++ blitter_angular/js/bleets/new.controller.js
@@ -3,13 +3,15 @@
 (function(){
   angular
     .module("bleets")
-    .controller("BleetNewController", ["BleetFactory", BleetNewControllerFunction])
+    .controller("BleetNewController", ["BleetFactory", "$state", BleetNewControllerFunction])
 
-function BleetNewControllerFunction(BleetFactory){
+function BleetNewControllerFunction(BleetFactory, $state){
   console.log("created new bleet");
   this.bleet = new BleetFactory();
   this.create = function(){
-    this.bleet.$save()
+    this.bleet.$save().then(function(response){
+      $state.go("bleetIndex", ({id: response.id}));
+    })
   }
 }
 
